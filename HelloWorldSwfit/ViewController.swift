328d6178551e59eec81a755a263db969ed912058
Sagar Punhani <SagarPunhani@Sagars-MacBook-Pro-2.local>
2014-09-05 11:31:57 -0700

    Initial Commit

---
+++ HelloWorldSwfit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldSwfit
+//
+//  Created by Sagar Punhani on 9/5/14.
+//  Copyright (c) 2014 Sagar Punhani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

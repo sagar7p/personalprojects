69e4345ccacb35f4530a116b9173ba0583e0b99b
SagarPunhani <sagar7p@gmail.com>
2014-10-29 00:03:04 -0700

    My First App

--- HelloWorldSwfit/ViewController.swift
+++ HelloWorldSwfit/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWorldAction(nameTextField: UITextField) {
+        if nameTextField.text == "Sampurna" {
+            nameLabel.text = "You Suck"
+        }
+        else{
+        nameLabel.text = "Hello \(nameTextField.text)"
+        }
+    }
 
 }
 
